dc9e78d3b3718cdfb1a3fd66070e6751f9bb2f88
shishir <66941159+shiX0@users.noreply.github.com>
2025-01-22 10:59:29 +0545

    add mongoose dependency and integrate error handling middleware

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const { AppError } = require('./middlewares/ErrorHandler')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.use(AppError)
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
